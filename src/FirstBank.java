13f655055641276314b5c00e5e8bc556c3eb23b1
Samuel Almeida <barbosasamuel24@gmail.com>
2022-02-16 10:45:44 -0300

    finished code

---
+++ src/FirstBank.java
@@ -0,0 +1,14 @@
+public class FirstBank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account samuel = new Account("Samuel Almeida", "A00001");
+
+        Account vitoria = new Account("Vitoria Almeida", "A00002");
+
+        vitoria.showMenu();
+
+
+    }
+}
